552adab5321dfee7ab76672a846843443cbb3010
graanton <79745423+graanton@users.noreply.github.com>
2022-06-07 15:53:03 +0700

    30% pause menu

--- Assets/Tests/CanvasMethods.cs
+++ Assets/Tests/CanvasMethods.cs
@@ -3,59 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace TestScripts
+
+public class CanvasMethods : MonoBehaviour
 {
-    public class CanvasMethods : MonoBehaviour
+    [SerializeField] private KeyCode _pauseKey = KeyCode.Escape;
+    [SerializeField] private GameObject _pauseMenuPlanel;
+    [SerializeField] private GameObject _optionsPlanel;
+    [SerializeField] private Button _continueButton;
+    [SerializeField] private Button _optionsButton;
+    [SerializeField] private Button _exitButton;
+
+    private bool _isPause = false;
+
+    private void Start()
     {
-        [SerializeField] private KeyCode _pauseKey = KeyCode.Escape;
-        [SerializeField] private GameObject _pauseMenuPlanel;
-        [SerializeField] private GameObject _optionsPlanel;
-        [SerializeField] private Button _continueButton;
-        [SerializeField] private Button _optionsButton;
-        [SerializeField] private Button _exitButton;
+        ButtonsOnClickInit();
+        _pauseMenuPlanel.SetActive(false);
+    }
 
-        private bool _isPause = false;
-
-        private void Start()
+    private void Update()
+    {
+        if (Input.GetKeyDown(_pauseKey))
         {
-            ButtonsOnClickInit();
-            _pauseMenuPlanel.SetActive(false);
+            SwitchPauseState();
         }
+    }
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(_pauseKey))
-            {
-                SwitchPauseState();
-            }
-        }
+    private void ButtonsOnClickInit()
+    {
+        _continueButton.onClick.AddListener(SwitchPauseState);
+        _optionsButton.onClick.AddListener(OpenOptions);
+    }
 
-        private void ButtonsOnClickInit()
+    private void SwitchPauseState()
+    {
+        _isPause = !_isPause;
+        _pauseMenuPlanel.SetActive(_isPause);
+        if (_isPause)
         {
-            _continueButton.onClick.AddListener(SwitchPauseState);
-            _optionsButton.onClick.AddListener(OpenOptions);
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
         }
+        else
+        {
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
 
-        private void SwitchPauseState()
-        {
-            _isPause = !_isPause;
-            _pauseMenuPlanel.SetActive(_isPause);
-            if (_isPause)
-            {
-                Time.timeScale = 0;
-                Cursor.lockState = CursorLockMode.None;
-            }
-            else
-            {
-                Time.timeScale = 1;
-                Cursor.lockState = CursorLockMode.Locked;
-            }
-        }
-
-        private void OpenOptions()
-        {
-            _optionsPlanel.SetActive(true);
-        }
+    private void OpenOptions()
+    {
+        _optionsPlanel.SetActive(true);
     }
 }
 
